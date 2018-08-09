cfc3042b61c73c8f343693f8b922fea967b68928
Lawrence Wachira <lawrence.wachira@andela.com>
2018-08-09 09:16:05 +0300

    [Review] Review intial project code
    - Fix PEP8 violations
    - Update project code to effectively use marshamallow schemas
    - Add minor code refactoring

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from app import manager, app
+from app import manager
+
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()  # You need to run the manager instance to be able to
+    # perform migrations. You also now need to do `python app.py runserver`
+    # to run the inbuilt flask server. Alternatively, `flask run` also works
+    # as well and will load your environment variables in .flaskenv and .env
+    # automatically. See: https://github.com/pallets/flask/pull/2416
--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import os
+
 from flask import Flask
 from flask_restplus import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
@@ -7,8 +8,14 @@ from flask_script import Manager
 
 from app import config
 
+
 app = Flask(__name__)
-app.config.from_object(config.DevelopmentConfig)
+# The environment variable below helps to allow firing up a server with
+# of a given configuration without having to edit the code itself. Just your
+# environment variables. It also has a default 'production' value
+# when it's not set in the environment.
+environment = os.getenv('FLASK_ENV', 'production')
+app.config.from_object(config.configuration[environment])
 api = Api(app)
 db = SQLAlchemy(app)
 
